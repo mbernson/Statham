b1330f1fb734bd86047e8e58552bb6b659e20ffd
Mathijs Bernson <mathijs.bernson@gmail.com>
2016-10-27 18:28:51 +0200

    Add support for Carthage

---
+++ Statham/Statham.h
@@ -0,0 +1,19 @@
+//
+//  Statham.h
+//  Statham
+//
+//  Created by Mathijs Bernson on 27/10/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Statham.
+FOUNDATION_EXPORT double StathamVersionNumber;
+
+//! Project version string for Statham.
+FOUNDATION_EXPORT const unsigned char StathamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Statham/PublicHeader.h>
+
+
